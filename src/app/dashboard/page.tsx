93949eeeb7da879ddb5366723b90e94ca6fde041
xee95 <zeeshannawaz2017@gmail.com>
2023-10-17 20:17:05 +0500

    trpc and prisma added to project

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import DashboadComponent from "@/components/Dashboard";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Dashboard = async() => {
+
+    const { getUser } = getKindeServerSession()
+    const user = getUser();
+
+    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+
+    
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id
+    }
+  })
+
+  if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+    return ( 
+        <DashboadComponent/>
+     );
+}
+ 
+export default Dashboard;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +19,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
+      <Providers>
       <body
         className={cn(
           "min-h-screen font-sans antialiased grainy",
           inter.className
         )}
       >
+        <Navbar/>
         {children}
       </body>
+      </Providers>
     </html>
   );
 }
